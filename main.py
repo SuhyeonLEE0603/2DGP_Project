1d0cbc0efdd93d418e38922eae6b86b8c1c60fc3
Lee Su Hyeon <dltngus12323@gmail.com>
2023-10-31 11:37:55 +0900

    LEC 13 게임프레임웍 적용 준비

--- main.py
+++ main.py
@@ -49,3 +49,16 @@ while running:
 
 # finalization code
 close_canvas()
+
+
+# lec13 게임 프레임웍 적용준비
+# from pico2d import open_canvas, close_canvas
+# import game_framework
+# # import logo_mode as start_mode
+# # import title_mode as start_mode
+# import play_mode as start_mode
+#
+# open_canvas()
+# game_framework.run(start_mode)
+# close_canvas()
+
